5bed0b152a327a18e342af276c005bfddc08af94
Vladimir Kirienko <vv90@mechmind.net>
2016-09-10 02:35:18 +0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Vladimir on 9/10/2016.
+ */
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var path = require('path');
+var webpack = require('webpack');
+var webpackConfig = require('../webpack.config.dev');
+
+
+var app = express();
+var port = 8080;
+
+app.use(require('webpack-dev-middleware')(webpack(webpackConfig), {
+	publicPath: webpackConfig.output.publicPath,
+	stats: { colors: true }
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var router = express.Router();
+
+router.use(function (req, res, next) {
+	console.log(req.method + ': ' + req.url);
+	next();
+});
+
+router.get('/', function(req, res) {
+	res.json({message: 'api is working'});
+});
+
+mongoose.connect('mongodb://localhost/RaspberryJam');
+
+app.use('/api', router);
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, '../index.html'))
+});
+
+app.listen(port);
+console.log('server started on port ' + port);
---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Vladimir on 9/10/2016.
+ */
+module.exports = {
+	debug: true,
+	devtool: "source-map",//"cheap-module-eval-source-map",
+	entry:  __dirname + "/app/main.js",
+	output: {
+		path: __dirname + "/build",
+		publicPath: "/",
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node-modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			},
+			{
+				test: /(\.css)$/, loaders: ['style', 'css']
+			}
+		]
+	}
+};
